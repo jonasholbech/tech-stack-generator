940426ee7e3bf7d262f09ed7c1574e8f86ec264b
Jonas Holbech <jh@jonasholbech.dk>
2024-03-21 22:21:09 +0100

    fooling around

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+  import Stack from './lib/Stack.svelte'
+  const frontends = ["Svelte", "React", "Vue", "jQuery","Angular", "Preact", "Solid", "Lit", "Alpine.js", "HTML/JS", "Astro", "Marco"]
+  const bundlers = ["Vite", "Webpack", "Rollup", "Parcel", "Parcel 2", "Gulp", "Grunt"]
+  const styles = ["Sass", "PostCSS", "Vanilla", "Tailwind", "MUI", "jQuery UI", "Bootstrap", "Materialize", "Bulma", "Foundation", "Semantic UI", "UIKit"]
+  const backends = ["Node", "Express", "Fastify", "Koa", "Hapi", "WordPress", "Drupal", "Umbraco", "Laravel", "Django", "Ruby on Rails", "REST", "GraphQL", "Flat file", "PHP"]
+  const deployments = ["Old-school FTP", "GitHub Pages", "Netlify", "Vercel", "Cloudflare Pages", "Render", "Heroku", "Now", "AWS", "GCP", "Azure", "Fly.io"]
+  
+  
+  const metas = ["SvelteKit", "Sapper", "Nuxt", "Next.js", "Gatsby"]
+  const tools = ["TypeScript", "ESLint", "Prettier"]
+</script>
+
+<main>
+  <h1>Tech-Stack-Generator</h1>
+  <p class="bold">How about a:</p>
+  <div class="row">
+    <Stack items={frontends}/> frontend
+  </div>
+  <div class="row">
+    with <Stack items={bundlers}/> under the hood
+  </div>
+  <div class="row">
+  styled with <Stack items={styles}/>
+  </div>
+  <div class="row">
+  using a <Stack items={backends}/> backend
+  </div>
+  <div class="row">
+    deployed on <Stack items={deployments}/>
+  </div>
+</main>
+
+
---
+++ src/lib/Stack.svelte
@@ -0,0 +1,47 @@
+<script>
+    export let items;
+    export let run=false;
+    let target=0;
+    function toggle(){
+        const nextTarget = Math.floor(Math.random()*items.length+1);
+        if(nextTarget===target){
+            toggle()
+        } else {
+            target = nextTarget
+        }
+    }
+</script>
+<div class="wrapper">
+<ul class="run" style="--target: {target}">
+    <li on:click={toggle}>???</li>
+    {#each items as item}
+        <li on:click={toggle}>{item}</li>
+    {/each}
+</ul>
+</div>
+<style>
+    div.wrapper {
+        display:inline-block;
+        height:calc(var(--base-size)*2 + 1px);
+        overflow:hidden;
+        vertical-align:bottom;
+    }
+    ul {
+        list-style: none;
+        vertical-align:bottom;
+    }
+    li {
+        padding-top: var(--base-size);
+        border-bottom:1px solid hotpink;
+        text-align:center;
+        cursor:pointer;
+    }
+    li:hover {
+        color:hotpink;
+    }
+    
+    .run {
+        transition: all 1s;
+        transform:translateY(calc(var(--target) * -2rem + var(--target) * -1px));
+    }
+</style>
